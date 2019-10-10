3ea67e09003a5f38663e7e229f6dda3d33e770ee
Sebastian Mößler <s.moeszler@binsoul.de>
2019-10-10 10:58:20 +0200

    Update dependencies and fix unit tests

--- tests/IntlNumberFormatterTest.php
+++ tests/IntlNumberFormatterTest.php
@@ -4,14 +4,25 @@ namespace BinSoul\Test\Common\I18n;
 
 use BinSoul\Common\I18n\DefaultLocale;
 use BinSoul\Common\I18n\Intl\IntlNumberFormatter;
+use PHPUnit\Framework\TestCase;
 
-class IntlNumberFormatterTest extends \PHPUnit_Framework_TestCase
+class IntlNumberFormatterTest extends TestCase
 {
+    /* NARROW NO BREAK SPACE */
+    const NNBS = "\xE2\x80\xAF";
+
+    /* NO BREAK SPACE */
+    const NBS = "\xC2\xA0";
+
+    /* RIGHT-TO-LEFT MARK */
+    const RTLM = "\xE2\x80\x8F";
+
+    /* ARABIC LETTER MARK */
+    const ALM = "\xD8\x9C";
+
     public function test_formats_decimal_numbers()
     {
         $formatter = new IntlNumberFormatter(DefaultLocale::fromString('de-DE'));
-        $this->assertEquals('', $formatter->formatDecimal(''));
-        $this->assertEquals('', $formatter->formatDecimal(null));
         $this->assertEquals('-1.001,12345', $formatter->formatDecimal(-1001.12345));
         $this->assertEquals('1.001,12345', $formatter->formatDecimal(1001.12345));
         $this->assertEquals('100.000.001,12345', $formatter->formatDecimal(100000001.12345));
@@ -22,7 +33,7 @@ class IntlNumberFormatterTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('100,000,001.12345', $formatter->formatDecimal(100000001.12345));
 
         $formatter = new IntlNumberFormatter(DefaultLocale::fromString('ar-EG'));
-        $this->assertEquals("\xE2\x80\x8F-١٬٠٠١٫١٢٣٤٥", $formatter->formatDecimal(-1001.12345));
+        $this->assertEquals(self::ALM.'-١٬٠٠١٫١٢٣٤٥', $formatter->formatDecimal(-1001.12345));
         $this->assertEquals('١٬٠٠١٫١٢٣٤٥', $formatter->formatDecimal(1001.12345));
         $this->assertEquals('١٠٠٬٠٠٠٬٠٠١٫١٢٣٤٥', $formatter->formatDecimal(100000001.12345));
 
@@ -35,35 +46,32 @@ class IntlNumberFormatterTest extends \PHPUnit_Framework_TestCase
     public function test_formats_percents()
     {
         $formatter = new IntlNumberFormatter(DefaultLocale::fromString('de-DE'));
-        $this->assertEquals('', $formatter->formatPercent(''));
-        $this->assertEquals('', $formatter->formatPercent(null));
-        $this->assertEquals("-1.001,12\xc2\xa0%", $formatter->formatPercent(-1001.12345, 2));
-        $this->assertEquals("-1.001,12345\xc2\xa0%", $formatter->formatPercent(-1001.12345));
-        $this->assertEquals("1.001,12345\xc2\xa0%", $formatter->formatPercent(1001.12345));
-        $this->assertEquals("100.000.001,12345\xc2\xa0%", $formatter->formatPercent(100000001.12345));
+        $this->assertEquals('-1.001,12'.self::NBS.'%', $formatter->formatPercent(-1001.12345, 2));
+        $this->assertEquals('-1.001,12345'.self::NBS.'%', $formatter->formatPercent(-1001.12345));
+        $this->assertEquals('1.001,12345'.self::NBS.'%', $formatter->formatPercent(1001.12345));
+        $this->assertEquals('100.000.001,12345'.self::NBS.'%', $formatter->formatPercent(100000001.12345));
 
         $formatter = new IntlNumberFormatter(DefaultLocale::fromString('ar-EG'));
-        $this->assertEquals("\xE2\x80\x8F-١٬٠٠١٫١٢٣٤٥٪", $formatter->formatPercent(-1001.12345));
-        $this->assertEquals('١٬٠٠١٫١٢٣٤٥٪', $formatter->formatPercent(1001.12345));
-        $this->assertEquals('١٠٠٬٠٠٠٬٠٠١٫١٢٣٤٥٪', $formatter->formatPercent(100000001.12345));
-    }
 
+        $this->assertEquals(self::ALM.'-١٬٠٠١٫١٢٣٤٥٪'.self::ALM, $formatter->formatPercent(-1001.12345));
+        $this->assertEquals('١٬٠٠١٫١٢٣٤٥٪'.self::ALM, $formatter->formatPercent(1001.12345));
+        $this->assertEquals('١٠٠٬٠٠٠٬٠٠١٫١٢٣٤٥٪'.self::ALM, $formatter->formatPercent(100000001.12345));
+    }
 
     public function test_formats_currency()
     {
         $formatter = new IntlNumberFormatter(DefaultLocale::fromString('de-DE'));
-        $this->assertEquals('', $formatter->formatCurrency('', ''));
-        $this->assertEquals('', $formatter->formatCurrency(null, ''));
         $this->assertEquals('1.001,12', $formatter->formatCurrency(1001.12345));
 
-        $this->assertEquals("-1.001,12\xc2\xa0€", $formatter->formatCurrency(-1001.12345, 'EUR'));
-        $this->assertEquals("1.001,12\xc2\xa0€", $formatter->formatCurrency(1001.12345, 'EUR'));
-        $this->assertEquals("100.000.001,12\xc2\xa0€", $formatter->formatCurrency(100000001.12345, 'EUR'));
+        $this->assertEquals('-1.001,12'.self::NBS.'€', $formatter->formatCurrency(-1001.12345, 'EUR'));
+        $this->assertEquals('1.001,12'.self::NBS.'€', $formatter->formatCurrency(1001.12345, 'EUR'));
+        $this->assertEquals('100.000.001,12'.self::NBS.'€', $formatter->formatCurrency(100000001.12345, 'EUR'));
 
         $formatter = new IntlNumberFormatter(DefaultLocale::fromString('fr-FR'));
-        $this->assertEquals("-1\xc2\xa0001,12\xc2\xa0€", $formatter->formatCurrency(-1001.12345, 'EUR'));
-        $this->assertEquals("1\xc2\xa0001,12\xc2\xa0€", $formatter->formatCurrency(1001.12345, 'EUR'));
-        $this->assertEquals("100\xc2\xa0000\xc2\xa0001,12\xc2\xa0€", $formatter->formatCurrency(100000001.12345, 'EUR'));
+
+        $this->assertEquals('-1'.self::NNBS.'001,12'.self::NBS.'€', $formatter->formatCurrency(-1001.12345, 'EUR'));
+        $this->assertEquals('1'.self::NNBS.'001,12'.self::NBS.'€', $formatter->formatCurrency(1001.12345, 'EUR'));
+        $this->assertEquals('100'.self::NNBS.'000'.self::NNBS.'001,12'.self::NBS.'€', $formatter->formatCurrency(100000001.12345, 'EUR'));
 
         $formatter = new IntlNumberFormatter(DefaultLocale::fromString('en-US'));
         $this->assertEquals('-€1,001.12', $formatter->formatCurrency(-1001.12345, 'EUR'));
