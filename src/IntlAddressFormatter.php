068063abbe5573222a9646c411b8324eb6782cd2
Sebastian Mößler <s.moeszler@binsoul.de>
2019-11-26 19:08:20 +0100

    Add implementation of the AddressFormatter interface

---
+++ src/IntlAddressFormatter.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BinSoul\Common\I18n\Intl;
+
+use BinSoul\Common\I18n\DefaultAddressFormatter;
+use BinSoul\Common\I18n\DefaultLocale;
+use BinSoul\Common\I18n\Locale;
+
+/**
+ * Formats addresses using the PHP intl extension.
+ */
+class IntlAddressFormatter extends DefaultAddressFormatter
+{
+    public function __construct(?Locale $locale = null)
+    {
+        parent::__construct($locale ?? DefaultLocale::fromString(\Locale::getDefault()));
+    }
+
+    protected function isoCodeToName(string $isoCode): string
+    {
+        return \Locale::getDisplayRegion('-'.$isoCode, $this->locale->getCode());
+    }
+}
---
+++ tests/IntlAddressFormatterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace BinSoul\Test\Common\I18n;
+
+use BinSoul\Common\I18n\DefaultAddress;
+use BinSoul\Common\I18n\DefaultLocale;
+use BinSoul\Common\I18n\Intl\IntlAddressFormatter;
+use PHPUnit\Framework\TestCase;
+
+class IntlAddressFormatterTest extends TestCase
+{
+    public function countriesAndNames():array
+    {
+        return [
+            ['DE', 'en', 'Germany'],
+            ['DE', 'fr', 'Allemagne'],
+            ['DE', 'de', 'Deutschland'],
+            ['IT', 'en', 'Italy'],
+            ['IT', 'fr', 'Italie'],
+            ['IT', 'de', 'Italien'],
+            ['FR', 'en', 'France'],
+            ['FR', 'fr', 'France'],
+            ['FR', 'de', 'Frankreich'],
+        ];
+    }
+    /**
+     * @dataProvider countriesAndNames
+     */
+    public function test_generates_localized_country_names(string $countryCode, string $localeCode, string $countryName): void
+    {
+        $formatter = new IntlAddressFormatter(DefaultLocale::fromString($localeCode));
+        $address = $this->buildAddress($countryCode);
+
+        $formattedAddress = $formatter->format($address);
+        $lines = explode("\n", $formattedAddress);
+        $this->assertEquals($countryName, $lines[count($lines) - 1]);
+    }
+
+    /**
+     * @return DefaultAddress
+     */
+    private function buildAddress(string $countryCode): DefaultAddress
+    {
+        return new DefaultAddress(
+            'organization',
+            'namePrefix',
+            'firstname',
+            'lastname',
+            'addressLine1',
+            'addressLine2',
+            'addressLine3',
+            'sortingCode',
+            'postalCode',
+            'locality',
+            'dependentLocality',
+            'adminArea',
+            $countryCode
+        );
+    }
+}
